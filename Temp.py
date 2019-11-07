f9ead0e88b495bb20f35a135b4214f66410495e8
Ahmet Turgut <hahahayesss@gmail.com>
2019-11-07 20:33:57 +0300

    second

--- Temp.py
+++ Temp.py
@@ -1,17 +1,9 @@
 import numpy as np
 import pandas as pd
 
-raw_data = pd.read_csv("jobs.csv")
-time = np.zeros(raw_data.shape)
-collected = np.zeros(raw_data.shape)
-file_name = np.zeros(raw_data.shape)
+raw_list = pd.read_csv("_jobs.csv").values
+total = 0
+for data in raw_list:
+    total += data[3]
 
-raw_data["time"] = time
-raw_data["collected"] = collected
-raw_data["file_name"] = file_name
-
-raw_data = raw_data.astype({"time": "int64",
-                            "collected": "int64",
-                            "file_name": "int64"})
-
-raw_data.to_csv("_jobs.csv")
+print("Total:", total)

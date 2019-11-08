aebb24b77a19e676dcee669744fdf72ade105104
Ahmet's fake <qjpcxo@gmail.com>
2019-11-08 20:48:21 +0000

    second

--- linkedin/mk2/CollectData.py
+++ linkedin/mk2/CollectData.py
@@ -1,15 +1,43 @@
+import os
+import time
 import json
 import click
+import send2trash
 import pandas as pd
 from scrape_linkedin import ProfileScraper
 
-LI_AT = "AQEDAR6aCHoBhwSZAAABbkyLWfoAAAFucJfd-k0ArZHaZqjXWBbEEKGLG-PnXBn7RJTznENgkJDVL4Q2A1DfLaQU8ixioS5hgXNdQsQb6sS5blKQMhwvfqGQssdcQNBCio6klE5yXWHNBlxOmp95OfUd"
+LI_AT = ""
 
 
 def _collect_data(profile_link):
-    with ProfileScraper(cookie=LI_AT) as scraper:
-        profile = scraper.scrape(user=profile_link)
-    return profile.to_dict()
+    try:
+        with ProfileScraper(cookie=LI_AT) as scraper:
+            profile = scraper.scrape(user=profile_link)
+        return profile.to_dict()
+    except:
+        return {"notFound": True, "userLink": profile_link}
+
+
+def _write_temp_file(_temp_list, _temp_folder):
+    if not os.path.exists(_temp_folder):
+        os.mkdir(_temp_folder)
+
+    _temp_file_name = str(time.time()) + ".q"
+    with open(os.path.join(_temp_folder, _temp_file_name), "w") as _temp_file:
+        json.dump(_temp_list, _temp_file)
+
+
+def _write_output(output, _temp_folder):
+    _temp_files = [f for f in os.listdir(_temp_folder) if f.__contains__(".q")]
+    _temp_list = []
+    for _temp in _temp_files:
+        with open(os.path.join(_temp_folder, _temp)) as _temp_file:
+            _temp_list += json.load(_temp_file)
+
+    with open(output, "w") as json_file:
+        json.dump(_temp_list, json_file)
+
+    send2trash.send2trash(_temp_folder)
 
 
 @click.command()
@@ -19,17 +47,28 @@ def _collect_data(profile_link):
 @click.option("--output_json", "-o",
               default=r"../../json_files/Graphic Designer.json",
               help="")
-def start(input_csv, output_json):
+@click.option("--temp_folder", "-t",
+              default=r"_temp",
+              help="")
+def start(input_csv, output_json, temp_folder):
     raw_list = pd.read_csv(input_csv).values
     data_list = raw_list[:, 1]
 
+    _process_percent_size = int(len(data_list) / 100)
+    _process_counter = 0
+
     profile_data_list = []
-    for link in data_list:
+    for index, link in enumerate(data_list):
         profile_data_list.append(
             _collect_data(link.rsplit("/", 1)[1]))
+        if len(profile_data_list) == 10:
+            _write_temp_file(profile_data_list, temp_folder)
+            profile_data_list.clear()
+        if index % _process_percent_size == 0:
+            print("%" + str(_process_counter))
+            _process_counter += 1
 
-    with open(output_json, "w") as json_file:
-        json.dump(profile_data_list, json_file)
+    _write_output(output_json, temp_folder)
 
 
 if __name__ == '__main__':
